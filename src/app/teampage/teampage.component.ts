3fb22edf7807ad83f140b32d7139be07bae3edea
Alex Muench <ammuench@gmail.com>
2016-10-06 00:59:47 -0500

    Add 'row' to divs.  Because I forgot how to bootstrap.

--- src/app/teampage/teampage.component.ts
+++ src/app/teampage/teampage.component.ts
@@ -26,6 +26,38 @@ export class TeampageComponent implements OnInit {
       URL: "http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=224474",
       pID: 224474,
       image: "http://static.donordrive.com/clients/extralife/img/avatar-constituent-default.gif"
+    },
+    {
+      name: "Alex Muench",
+      isTeamCaptain: true,
+      raised: 0,
+      URL: "http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=219449",
+      pID: 219449,
+      image: "http://assets.donordrive.com/extralife/images/$avatars$/constituent_0C07ECD7-C293-34EB-45A3F7B77F8BA043.jpg"
+    },
+    {
+      name: "michael majewski",
+      isTeamCaptain: false,
+      raised: 0,
+      URL: "http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=224474",
+      pID: 224474,
+      image: "http://static.donordrive.com/clients/extralife/img/avatar-constituent-default.gif"
+    },
+    {
+      name: "Alex Muench",
+      isTeamCaptain: true,
+      raised: 0,
+      URL: "http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=219449",
+      pID: 219449,
+      image: "http://assets.donordrive.com/extralife/images/$avatars$/constituent_0C07ECD7-C293-34EB-45A3F7B77F8BA043.jpg"
+    },
+    {
+      name: "michael majewski",
+      isTeamCaptain: false,
+      raised: 0,
+      URL: "http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=224474",
+      pID: 224474,
+      image: "http://static.donordrive.com/clients/extralife/img/avatar-constituent-default.gif"
     }
   ]
 
